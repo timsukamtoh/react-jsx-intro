25a4b4bb198153484d6d4dd5e04a3f30555e1a68
Kenny Burgher <kenny.burgher@gmail.com>
2023-05-22 12:07:49 -0700

    part 3

--- part-2/Tweet.js
+++ part-2/Tweet.js
@@ -1,7 +1,7 @@
 function Tweet({username, name, date, message}) {
   return (
     <div>
-      <p>{username} ({name}):</p>
+      <p className="text-red">{username} ({name}):</p>
       <p>{message}</p>
       <p>{date.toString()}</p>
     </div>
